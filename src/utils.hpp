1f8315a86bf93cc9d4e94c455b3e8f82cadd8379
GDWR <gregory.dwr@gmail.com>
2023-03-13 16:25:37 +0000

    Add utils to get integer to string

---
+++ src/utils.hpp
@@ -0,0 +1,26 @@
+#ifndef GTXT_UTILS_HPP
+#define GTXT_UTILS_HPP
+
+#include <string>
+#include <algorithm>
+
+std::string integer_to_string_representation(int integer) {
+    std::string output;
+
+    while (true) {
+        int num = integer % 10;
+        integer = integer / 10;
+
+        output += '1' + (num - 1);
+
+        if (integer == 0) {
+            break;
+        }
+    }
+
+    std::reverse(output.begin(), output.end());
+    return output;
+}
+
+
+#endif //GTXT_UTILS_HPP
